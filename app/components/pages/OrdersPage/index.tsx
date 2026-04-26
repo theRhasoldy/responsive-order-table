2563cfb7b9e76388b96b503273ec8950c4ad4676
Nour El Din Shady <rhasoldy@gmail.com>
2026-04-27 00:39:37 +0300

    feat: integrate search and status filters into Orders page
    - Update OrdersPage to include FilterSearchInput and FilterSelect.
    - Implement client-side filtering logic using search parameters.
    - Adjust page layout for better responsiveness and visual hierarchy.

--- app/components/pages/OrdersPage/index.tsx
+++ app/components/pages/OrdersPage/index.tsx
@@ -1,12 +1,59 @@
 import DataTable from "~/components/widgets/DataTable";
 import { orders } from "./_data";
 import { columns } from "./_columns";
+import FilterSearchInput from "~/components/widgets/FilterSearchInput";
+import useUpdateSearchParams from "~/common/hooks/useUpdateSearchParams";
+import { useMemo } from "react";
+import FilterSelect from "~/components/widgets/FilterSelect";
+import { STATUS } from "~/common/constants/status";
+
+const filters = [
+  {
+    label: "Id",
+    value: "id",
+  },
+  {
+    label: "Customer Name",
+    value: "customerName",
+  },
+];
 
 function OrdersPage() {
+  const { searchParams } = useUpdateSearchParams();
+
+  const filteredData = useMemo(() => {
+    return orders.filter((order) => {
+      return Object.entries(searchParams).every(([key, value]) => {
+        return (
+          order[key as keyof typeof order].toString().toLowerCase() ==
+          value.toLowerCase()
+        );
+      });
+    });
+  }, [searchParams]);
+
   return (
     <>
-      <h1 className="text-3xl font-semibold mb-4">Orders</h1>
-      <DataTable data={orders} columns={columns} />
+      <div className="mb-4 space-y-4">
+        <h1 className="text-xl font-semibold">Orders</h1>
+        <div className="flex gap-4 flex-wrap">
+          <FilterSearchInput
+            className="w-xl"
+            filters={filters}
+            placeholder="Search orders"
+          />
+          <FilterSelect
+            className="w-full md:w-32"
+            filterKey="status"
+            filters={Object.values(STATUS).map((status) => ({
+              label: status,
+              value: status.toLowerCase(),
+            }))}
+            placeholder="Filter by"
+          />
+        </div>
+      </div>
+      <DataTable data={filteredData} columns={columns} />
     </>
   );
 }
